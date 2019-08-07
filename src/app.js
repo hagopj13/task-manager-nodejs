6a1fcf08d48aaf3ff6a6c838f9980ce8696ed309
Hagop Jamkojian <hagopj13@gmail.com>
2019-08-07 19:10:00 +0200

    feat: add cors, helmet, methodOverride, and compression middlewares
    closes #15

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const helmet = require('helmet');
+const compression = require('compression');
+const cors = require('cors');
+const methodOverride = require('method-override');
 require('./db/mongoose');
 const routes = require('./routes/v1');
 const { errorConverter, errorHandler, notFoundError } = require('./middlewares/error');
@@ -7,6 +11,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(compression());
+app.use(methodOverride());
+app.use(helmet());
+app.use(cors());
 
 app.use('/v1', routes);
 
