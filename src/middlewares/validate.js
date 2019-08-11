d9f4751c82d18514c5c6bbc68c6965c00fb322c0
Hagop Jamkojian <hagopj13@gmail.com>
2019-08-11 23:55:58 +0200

    refactor: remove unnecessary default value on validate schema

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -2,7 +2,7 @@ const Joi = require('@hapi/joi');
 const Boom = require('boom');
 const { pick } = require('lodash');
 
-const validate = (schema = {}) => (req, res, next) => {
+const validate = schema => (req, res, next) => {
   const validSchema = pick(schema, ['params', 'query', 'body']);
   const toValidate = pick(req, Object.keys(validSchema));
   Joi.validate(toValidate, validSchema, { abortEarly: false }, (err, value) => {
