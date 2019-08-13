6fbbc04907a061ac901cb95002120c813121b3df
Hagop Jamkojian <hagopj13@gmail.com>
2019-08-14 00:54:24 +0200

    test: add login api tests

--- src/tests/integration/auth.test.js
+++ src/tests/integration/auth.test.js
@@ -2,16 +2,13 @@ const mongoose = require('mongoose');
 const { expect } = require('chai');
 const request = require('supertest');
 const httpStatus = require('http-status');
+const { pick } = require('lodash');
 const app = require('../../app');
 const User = require('../../models/user.model');
 const { setupUsers } = require('./fixtures');
 const { userOne } = require('./fixtures/user.fixtures');
 
 describe('Auth Route', () => {
-  beforeEach(async () => {
-    await setupUsers();
-  });
-
   after(() => {
     mongoose.models = {};
     mongoose.modelSchemas = {};
@@ -26,6 +23,7 @@ describe('Auth Route', () => {
         password: 'White1234!',
         age: 22,
       };
+      await setupUsers();
     });
 
     const exec = async () => {
@@ -39,6 +37,7 @@ describe('Auth Route', () => {
       expect(response.status).to.be.equal(httpStatus.CREATED);
       delete newUser.password;
       expect(response.body.user).to.include(newUser);
+      expect(response.body.user).not.to.have.property('password');
       expect(response.body.user).to.have.property('id');
 
       const dbUser = await User.findById(response.body.user.id);
@@ -46,57 +45,49 @@ describe('Auth Route', () => {
       expect(dbUser.password).not.to.be.equal(newUser.password);
     });
 
-    it('should return an error if email is missing', async () => {
-      delete newUser.email;
+    const checkRegisterValidationError = async () => {
       const response = await exec();
       expect(response.status).to.be.equal(httpStatus.BAD_REQUEST);
+    };
+
+    it('should return an error if email is missing', async () => {
+      delete newUser.email;
+      await checkRegisterValidationError();
     });
 
     it('should return an error if email is invalid', async () => {
       newUser.email = 'notvalid';
-      const response = await exec();
-      expect(response.status).to.be.equal(httpStatus.BAD_REQUEST);
+      await checkRegisterValidationError();
     });
 
     it('should return an error if email is already used', async () => {
       newUser.email = userOne.email;
-      const response = await exec();
-      expect(response.status).to.be.equal(httpStatus.BAD_REQUEST);
+      await checkRegisterValidationError();
     });
 
     it('should return an error if password is missing', async () => {
       delete newUser.password;
-      const response = await exec();
-      expect(response.status).to.be.equal(httpStatus.BAD_REQUEST);
+      await checkRegisterValidationError();
     });
 
     it('should return an error if password is contains the word password', async () => {
       newUser.password = 'Red1234!password';
-      const response = await exec();
-      expect(response.status).to.be.equal(httpStatus.BAD_REQUEST);
-
-      // password shorter than 8 characters
-      newUser.password = 'Red1234';
-      const response3 = await exec();
-      expect(response3.status).to.be.equal(httpStatus.BAD_REQUEST);
+      await checkRegisterValidationError();
     });
 
     it('should return an error if password is shorter than 8 characters', async () => {
       newUser.password = 'Red1234';
-      const response = await exec();
-      expect(response.status).to.be.equal(httpStatus.BAD_REQUEST);
+      await checkRegisterValidationError();
     });
 
     it('should return an error if name is missing', async () => {
       delete newUser.name;
-      const response = await exec();
-      expect(response.status).to.be.equal(httpStatus.BAD_REQUEST);
+      await checkRegisterValidationError();
     });
 
     it('should return an error if age is less than 0', async () => {
       newUser.age = -1;
-      const response = await exec();
-      expect(response.status).to.be.equal(httpStatus.BAD_REQUEST);
+      await checkRegisterValidationError();
     });
 
     it('should set the age by default to 0 if not given', async () => {
@@ -104,6 +95,67 @@ describe('Auth Route', () => {
       const response = await exec();
       expect(response.status).to.be.equal(httpStatus.CREATED);
       expect(response.body.user.age).to.be.equal(0);
+    });
+  });
+
+  describe('POST /v1/auth/login', () => {
+    let loginCredentials;
+
+    beforeEach(() => {
+      loginCredentials = {
+        email: userOne.email,
+        password: userOne.password,
+      };
+    });
+
+    const exec = async () => {
+      return request(app)
+        .post('/v1/auth/login')
+        .send(loginCredentials);
+    };
+
+    it('should successfully login user when correct email and password are provided', async () => {
+      const response = await exec();
+      expect(response.status).to.be.equal(httpStatus.OK);
+
+      const dbUser = await User.findById(userOne._id);
+      expect(response.body.user).to.be.deep.equal(pick(dbUser, ['id', 'email', 'name', 'age']));
+    });
+
+    const checkLoginValidationError = async () => {
+      const response = await exec();
+      expect(response.status).to.be.equal(httpStatus.BAD_REQUEST);
+    };
+
+    it('should return a 400 error if email is missing', async () => {
+      delete loginCredentials.email;
+      await checkLoginValidationError();
+    });
+
+    it('should return a 400 error if password is missing', async () => {
+      delete loginCredentials.password;
+      await checkLoginValidationError();
+    });
+
+    const checkLoginAttemptError = async () => {
+      const response = await exec();
+      expect(response.status).to.be.equal(httpStatus.UNAUTHORIZED);
+      const expectedError = {
+        status: httpStatus.UNAUTHORIZED,
+        error: httpStatus[httpStatus.UNAUTHORIZED],
+        message: 'Incorrect email or password',
+      };
+      expect(response.body).to.be.deep.equal(expectedError);
+    };
+
+    it('should return a 401 error if user with such an email is not found', async () => {
+      loginCredentials.email = 'unknownEmail@example.com';
+      await checkLoginAttemptError();
+    });
+
+    it('should return a 401 error if user password is wrong', async () => {
+      loginCredentials.password = 'wrongPassword';
+      await checkLoginAttemptError();
     });
   });
 });
