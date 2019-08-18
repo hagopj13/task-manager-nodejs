591d07ff7fc6d2f7d830a56f1e0cf7c2f6b6c0d8
Hagop Jamkojian <hagopj13@gmail.com>
2019-08-18 17:12:55 +0300

    feat(auth): add env variables

--- src/config/config.js
+++ src/config/config.js
@@ -11,6 +11,11 @@ const envirnomentVariables = {
   port: process.env.PORT,
   mongodbUrl:
     process.env.NODE_ENV === 'test' ? process.env.MONDODB_TEST_URL : process.env.MONDODB_URL,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    accessExpirationMinutes: process.env.JWT_ACCESS_EXPIRATION_MINUTES,
+    refreshExpirationDays: process.env.JWT_REFRESH_EXPIRATION_DAYS,
+  },
 };
 
 module.exports = envirnomentVariables;
