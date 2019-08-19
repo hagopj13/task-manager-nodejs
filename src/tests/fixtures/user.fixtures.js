10d610589e554f3775918a98d7c3bc8f5b85540a
Hagop Jamkojian <hagopj13@gmail.com>
2019-08-19 23:34:43 +0300

    add token fixtures

--- src/tests/fixtures/user.fixtures.js
+++ src/tests/fixtures/user.fixtures.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
+const moment = require('moment');
+const jwt = require('jsonwebtoken');
+const { jwt: jwtConfig } = require('../../config/config');
 const User = require('../../models/user.model');
+const RefreshToken = require('../../models/refreshToken.model');
+
+const accessTokenExpires = moment().add(jwtConfig.accessExpirationMinutes, 'minutes');
+const refreshTokenExpires = moment().add(jwtConfig.refreshExpirationDays, 'days');
+const generateToken = (_id, expires) => {
+  const payload = {
+    sub: _id.toHexString(),
+    iat: moment().unix(),
+    exp: expires.unix(),
+  };
+  return jwt.sign(payload, jwtConfig.secret);
+};
 
 const userOneId = mongoose.Types.ObjectId();
 const userOne = {
@@ -7,6 +22,13 @@ const userOne = {
   name: 'User One',
   email: 'user1@example.com',
   password: 'Red1234!',
+};
+const userOneAccessToken = generateToken(userOneId, accessTokenExpires);
+const userOneRefreshToken = generateToken(userOneId, refreshTokenExpires);
+const userOneRefreshTokenObj = {
+  token: userOneRefreshToken,
+  user: userOneId,
+  expires: refreshTokenExpires.toDate(),
 };
 
 const userTwoId = mongoose.Types.ObjectId();
@@ -19,13 +41,17 @@ const userTwo = {
 
 const setupUsers = async () => {
   await User.deleteMany();
+  await RefreshToken.deteleMany();
   await new User(userOne).save();
   await new User(userTwo).save();
+  await new RefreshToken(userOneRefreshTokenObj).save();
 };
 
 module.exports = {
   userOneId,
   userOne,
+  userOneAccessToken,
+  userOneRefreshToken,
   userTwoId,
   userTwo,
   setupUsers,
