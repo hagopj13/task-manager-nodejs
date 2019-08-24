a9fbc38fd4c144d15d69aa6238bd08525741b900
Hagop Jamkojian <hagopj13@gmail.com>
2019-08-24 10:54:15 +0300

    add transform method to task

--- src/models/task.model.js
+++ src/models/task.model.js
@@ -1,20 +1,31 @@
 const mongoose = require('mongoose');
+const { pick } = require('lodash');
 
-const taskSchema = mongoose.Schema({
-  desctiption: {
-    type: String,
-    required: true,
+const taskSchema = mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
   },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+taskSchema.methods.transform = function() {
+  const task = this;
+  return pick(task, ['id', 'description', 'completed', 'owner']);
+};
 
 const Task = mongoose.model('Task', taskSchema);
 
