a2ef363e21d3c54a07b0690ceac656cb0cf6327a
Hagop Jamkojian <hagopj13@gmail.com>
2019-08-28 19:41:56 +0300

    avoid dropping the db because that is slower

--- src/tests/fixtures/index.js
+++ src/tests/fixtures/index.js
@@ -1,9 +1,13 @@
-const mongoose = require('mongoose');
+const User = require('../../models/user.model');
+const RefreshToken = require('../../models/refreshToken.model');
+const Task = require('../../models/task.model');
 const { setupUsers } = require('./user.fixture');
 const { setupTasks } = require('./task.fixture');
 
 const resetDatabase = async () => {
-  await mongoose.connection.db.dropDatabase();
+  await User.deleteMany();
+  await RefreshToken.deleteMany();
+  await Task.deleteMany();
   await setupUsers();
   await setupTasks();
 };
