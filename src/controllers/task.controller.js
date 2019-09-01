86e22ef066a3a0a7f90e1168c20f8c4579229996
Hagop Jamkojian <hagopj13@gmail.com>
2019-09-01 23:37:41 +0300

    reduce duplicate code

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -9,29 +9,28 @@ const createTask = catchAsync(async (req, res) => {
   res.status(httpStatus.CREATED).send(task.transform());
 });
 
-const getTask = catchAsync(async (req, res) => {
-  const task = await Task.findOne({ _id: req.params.taskId, owner: req.user._id });
+const getTaskOfUser = async (taskId, userId) => {
+  const task = await Task.findOne({ _id: taskId, owner: userId });
   if (!task) {
     throw Boom.notFound('Task not found');
   }
+  return task;
+};
+
+const getTask = catchAsync(async (req, res) => {
+  const task = await getTaskOfUser(req.params.taskId, req.user._id);
   res.send(task.transform());
 });
 
 const updateTask = catchAsync(async (req, res) => {
-  const task = await Task.findOne({ _id: req.params.taskId, owner: req.user._id });
-  if (!task) {
-    throw Boom.notFound('Task not found');
-  }
+  const task = await getTaskOfUser(req.params.taskId, req.user._id);
   Object.keys(req.body).forEach(update => (task[update] = req.body[update]));
   await task.save();
   res.send(task.transform());
 });
 
 const deleteTask = catchAsync(async (req, res) => {
-  const task = await Task.findOne({ _id: req.params.taskId, owner: req.user._id });
-  if (!task) {
-    throw Boom.notFound('Task not found');
-  }
+  const task = await getTaskOfUser(req.params.taskId, req.user._id);
   await task.remove();
   res.status(httpStatus.NO_CONTENT).send();
 });
