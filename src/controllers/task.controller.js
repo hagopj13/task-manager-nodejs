bf297a8735027e528b1144ee7138a3a0476bd21c
Hagop Jamkojian <hagopj13@gmail.com>
2019-09-11 00:26:29 +0200

    modify endpoints to allow admins to have access rights on any task

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -3,31 +3,31 @@ const { taskService } = require('../services');
 const { catchAsync } = require('../utils/controller.utils');
 
 const createTask = catchAsync(async (req, res) => {
-  const task = await taskService.createTask(req.body, req.user._id);
+  const task = await taskService.createTask(req.body, req.user);
   const response = task.transform();
   res.status(httpStatus.CREATED).send(response);
 });
 
 const getTasks = catchAsync(async (req, res) => {
-  const tasks = await taskService.getTasks(req.query, req.user._id);
+  const tasks = await taskService.getTasks(req.query, req.user);
   const response = tasks.map(task => task.transform());
   res.send(response);
 });
 
 const getTask = catchAsync(async (req, res) => {
-  const task = await taskService.getTask(req.params.taskId, req.user._id);
+  const task = await taskService.getTask(req.params.taskId, req.user);
   const response = task.transform();
   res.send(response);
 });
 
 const updateTask = catchAsync(async (req, res) => {
-  const task = await taskService.updateTask(req.params.taskId, req.body, req.user._id);
+  const task = await taskService.updateTask(req.params.taskId, req.body, req.user);
   const response = task.transform();
   res.send(response);
 });
 
 const deleteTask = catchAsync(async (req, res) => {
-  await taskService.deleteTask(req.params.taskId, req.user._id);
+  await taskService.deleteTask(req.params.taskId, req.user);
   res.status(httpStatus.NO_CONTENT).send();
 });
 
--- src/services/task.service.js
+++ src/services/task.service.js
@@ -2,36 +2,40 @@ const Boom = require('boom');
 const { Task } = require('../models');
 const { getQueryFilter, getQueryOptions } = require('../utils/service.util');
 
-const createTask = async (taskBody, userId) => {
-  const task = new Task({ ...taskBody, owner: userId });
+const createTask = async (taskBody, user) => {
+  const task = new Task({ ...taskBody, owner: user._id });
   await task.save();
   return task;
 };
 
-const getTask = async (taskId, userId) => {
-  const task = await Task.findOne({ _id: taskId, owner: userId });
+const getTask = async (taskId, user) => {
+  const filter = {
+    _id: taskId,
+    ...(user.role !== 'admin' && { owner: user._id }),
+  };
+  const task = await Task.findOne(filter);
   if (!task) {
     throw Boom.notFound('Task not found');
   }
   return task;
 };
 
-const updateTask = async (taskId, updateBody, userId) => {
-  const task = await getTask(taskId, userId);
+const updateTask = async (taskId, updateBody, user) => {
+  const task = await getTask(taskId, user);
   Object.keys(updateBody).forEach(update => (task[update] = updateBody[update]));
   await task.save();
   return task;
 };
 
-const deleteTask = async (taskId, userId) => {
-  const task = await getTask(taskId, userId);
+const deleteTask = async (taskId, user) => {
+  const task = await getTask(taskId, user);
   await task.remove();
   return task;
 };
 
-const getTasks = async (query, userId) => {
+const getTasks = async (query, user) => {
   const filter = getQueryFilter(query, ['completed']);
-  filter.owner = userId;
+  Object.assign(filter, user.role !== 'admin' ? { owner: user._id } : {});
   const options = getQueryOptions(query);
 
   const tasks = await Task.find(filter, null, options);
