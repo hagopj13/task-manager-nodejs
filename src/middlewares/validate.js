e3662a239ec076dd6696cd35ee06e6e4b3adb7b9
Hagop Jamkojian <hagopj13@gmail.com>
2019-09-15 23:26:04 +0200

    update joi

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -6,14 +6,16 @@ const { AppError } = require('../utils/error.util');
 const validate = schema => (req, res, next) => {
   const validSchema = pick(schema, ['params', 'query', 'body']);
   const toValidate = pick(req, Object.keys(validSchema));
-  Joi.validate(toValidate, validSchema, { abortEarly: false }, (err, value) => {
-    if (err) {
-      const errorMessage = err.details.map(details => details.message).join(', ');
-      return next(new AppError(httpStatus.BAD_REQUEST, errorMessage));
-    }
-    Object.assign(req, value);
-    return next();
-  });
+  const { value, error } = Joi.object()
+    .keys(validSchema)
+    .validate(toValidate);
+
+  if (error) {
+    const errorMessage = error.details.map(details => details.message).join(', ');
+    return next(new AppError(httpStatus.BAD_REQUEST, errorMessage));
+  }
+  Object.assign(req, value);
+  return next();
 };
 
 module.exports = validate;
