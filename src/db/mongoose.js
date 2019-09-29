0c504990e47f0edeab42e45b645645c39bf8fc08
Hagop Jamkojian <hagopj13@gmail.com>
2019-09-29 15:07:45 +0200

    show an error message if connection to db is refused

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 const { mongodbUrl } = require('../config/config');
+const logger = require('../config/logger');
 
-mongoose.connect(mongodbUrl, {
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(mongodbUrl, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch(error => logger.error(error.message));
