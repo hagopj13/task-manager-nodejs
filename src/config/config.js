cc9e4380acc2c3c78e5c3e267f63a6a305fa0428
Hagop Jamkojian <hagopj13@gmail.com>
2019-10-02 01:14:14 +0200

    change how env variables are loaded

--- src/config/config.js
+++ src/config/config.js
@@ -5,11 +5,19 @@ dotenv.config({
   path: path.join(__dirname, '../../.env'),
 });
 
+const getMongodbUrl = () => {
+  if (process.env.MONDODB_URL) {
+    return process.env.MONDODB_URL;
+  }
+  return process.env.NODE_ENV === 'test'
+    ? process.env.DEFAULT_MONDODB_TEST_URL
+    : process.env.DEFAULT_MONDODB_URL;
+};
+
 const envirnomentVariables = {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
-  mongodbUrl:
-    process.env.NODE_ENV === 'test' ? process.env.MONDODB_TEST_URL : process.env.MONDODB_URL,
+  mongodbUrl: getMongodbUrl(),
   jwt: {
     secret: process.env.JWT_SECRET,
     accessExpirationMinutes: process.env.JWT_ACCESS_EXPIRATION_MINUTES,
